1d545ebb6ecdaf30e644c13f973954bff58101b4
hshine1226 <hshine1226@gmail.com>
2020-07-28 02:10:08 +0900

    Add import scss

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
-import "./Movie.css";
+import "./Movie.scss";
 
 function Movie({ year, title, summary, poster, genres, url }) {
   return (
