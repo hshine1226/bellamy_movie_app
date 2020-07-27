02f6c9611551c98ba465d9706aa27e7f5266009c
hshine1226 <hshine1226@gmail.com>
2020-07-28 01:42:30 +0900

    Set HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import MovieDetail from "./routes/MovieDetail";
@@ -9,12 +9,12 @@ import "./reset.css";
 
 function App () {
   return (
-  <BrowserRouter>
+  <HashRouter>
     <Navigation />
     <Route path="/" exact component={Home}/>
     <Route path="/about" component={About}/>
     <Route path="/movie-detail" component={MovieDetail}/>
-  </BrowserRouter>)
+  </HashRouter>)
 }
 
 export default App;
