c1f32c197bfdc7482617c8ae686192c89be7efcf
hshine1226 <hshine1226@gmail.com>
2020-07-27 19:44:56 +0900

    Updates

---
+++ precache-manifest.a432277abb0a9c7f0044360baf325a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25cd08b8eb6b9d1df86c4cfc2b747b4",
+    "url": "/bellamy_movie_app/index.html"
+  },
+  {
+    "revision": "d351b3f2bec08c320b1d",
+    "url": "/bellamy_movie_app/static/css/main.0cb05812.chunk.css"
+  },
+  {
+    "revision": "2137992c13c6450c9e29",
+    "url": "/bellamy_movie_app/static/js/2.0f2041db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bellamy_movie_app/static/js/2.0f2041db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d351b3f2bec08c320b1d",
+    "url": "/bellamy_movie_app/static/js/main.75fb357b.chunk.js"
+  },
+  {
+    "revision": "e61c9f0c439e2c7b9476",
+    "url": "/bellamy_movie_app/static/js/runtime-main.3fca718f.js"
+  }
+]);
